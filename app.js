2a858730b3018eb3684e193ef3bb40e3b54b2185
jenanF <108689302+jenanF@users.noreply.github.com>
2024-12-08 18:08:27 +0300

    post done

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express = require('express')
 const app = express()
+app.use(express.json());
+
 
 const accounts = require('./accounts');
 
+const createNewAccount = (newAccountData) => {
+    const newId = accounts.length + 1;
+    const newFunds = newAccountData.funds ? newAccountData.funds : 0
+    const account = Object.assign({ id: newId }, newAccountData, { funds: newFunds })
+    console.log('new account is: ', account);
+    return account;
+
+}
+
 app.get('/accounts', (req, res) => {
     res.status(200).json(accounts)
+})
+
+app.post('/accounts', (req, res) => {
+    const newAccount = createNewAccount(req.body);
+    res.status(201).json(newAccount)
 })
 
 app.listen(8000, () => {
